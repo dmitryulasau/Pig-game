f78e7c378b33d1044742bb86c3e5844a0f0b4d54
DMITRY ULASAU <dmitrushok@gmail.com>
2022-01-19 00:11:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ btnHold.addEventListener('click', function () {
     // 2. Check if player's score is >= 100
 
     // Finish the game
-    if (scores[activePlayer] >= 4) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
